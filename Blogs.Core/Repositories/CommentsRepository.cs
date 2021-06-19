7a2a61a4774b2181ac365376fca6ab12aa496bb9
Fabio Vides <fabio.vides@outlook.com>
2021-06-19 17:32:07 -0500

    Added Implementations for IPostRepository and ICommentsRepostiroy

---
+++ Blogs.Core/Repositories/CommentsRepository.cs
@@ -0,0 +1,45 @@
+﻿using Blogs.Core.Abstract;
+using Blogs.Core.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blogs.Core.Repositories
+{
+    public class CommentsRepository : BaseRepository<BlogsContext>, ICommentRepository
+    {
+        public CommentsRepository(BlogsContext blogContext) : base(blogContext)
+        {
+
+        }
+
+        public async Task<List<Comment>> GetCommentsByPostId(Guid postId)
+        {
+            var result = await Db.Comments.Where(comment => comment.PostId == postId && !comment.IsDeleted).ToListAsync();
+            return result;
+        }
+
+        public async Task<IEnumerable<(Guid postId, IEnumerable<Comment> comments)>> GetCommentsByPostIds(Guid[] postIds)
+        {
+            var comments = await Db.Comments.Where(comment => postIds.Contains(comment.PostId) && !comment.IsDeleted).ToListAsync();
+            var groupedComments = comments.GroupBy(comment => comment.PostId, (postId, comments) => (postId, comments));
+            return groupedComments;
+        }
+
+        public async Task SaveComment(Guid postId, string text, Guid? userId)
+        {
+            var newComment = new Comment
+            {
+                PostId = postId,
+                Text = text,
+                WriterId = userId
+            };
+
+            Db.Comments.Add(newComment);
+
+            await Db.SaveChangesAsync();
+        }
+    }
+}
---
+++ Blogs.Core/Repositories/PostRepostiroy.cs
@@ -0,0 +1,64 @@
+﻿using Blogs.Core.Abstract;
+using Blogs.Core.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blogs.Core.Repositories
+{
+    public class PostRepostiroy : BaseRepository<BlogsContext>, IPostRepository
+    {
+        public PostRepostiroy(BlogsContext blogsContext) : base(blogsContext)
+        {
+
+        }
+
+        public async Task<List<Post>> GetPendingPostsByStatus(PostStatus status)
+        {
+            var posts = await Db.Posts.Where(post => post.StatusId == status && !post.IsDeleted).ToListAsync();
+            return posts;
+        }
+
+        public async Task<List<Post>> GetPostsByWriterId(Guid writerId)
+        {
+            var posts = await Db.Posts.Where(post => post.WriterId == writerId && !post.IsDeleted).ToListAsync();
+            return posts;
+        }
+
+        public async Task SavePost(string text, Guid writerId)
+        {
+            var newPost = new Post
+            {
+                Text = text,
+                WriterId = writerId
+            };
+            Db.Posts.Add(newPost);
+            await Db.SaveChangesAsync();
+        }
+
+        public async Task UpdatePost(Post updatedPost)
+        {
+            var originalPost = await Db.Posts.FirstOrDefaultAsync(post => post.Id == updatedPost.Id && !post.IsDeleted);
+            if (originalPost != null)
+            {
+                originalPost.StatusId = updatedPost.StatusId;
+                originalPost.WriterId = updatedPost.WriterId;
+                originalPost.ApprovalDate = updatedPost.ApprovalDate;
+                originalPost.Text = updatedPost.Text;
+                await Db.SaveChangesAsync();
+            }
+        }
+
+        public async Task SoftDeletePost(Guid postId)
+        {
+            var originalPost = await Db.Posts.FirstOrDefaultAsync(post => post.Id == postId && !post.IsDeleted);
+            if (originalPost != null)
+            {
+                originalPost.IsDeleted = true;
+                await Db.SaveChangesAsync();
+            }
+        }
+    }
+}
